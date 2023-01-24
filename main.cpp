88196fad2ac7e80b89993c8f21205c0813aa6ad9
N1kkoman <assasinjok@gmail.com>
2023-01-24 21:58:33 +0200

    add

---
+++ main.cpp
@@ -0,0 +1,86 @@
+﻿#include <SFML/Graphics.hpp>
+#include <string>
+#include <iostream>
+#include <thread>
+#include "windows.h"
+
+class AnimatedText
+{
+public:
+    AnimatedText(std::string text, float duration)
+    {
+        m_text = text;
+        m_duration = duration;
+    }
+
+    std::string getDisplayedText() const
+    {
+        return m_text.substr(0, currentChar);
+    }
+
+    int update() 
+    {
+        currentChar++;
+        return currentChar;
+    }
+
+    void DrawInConsole()
+    {
+        for (int i = 0; i < m_text.length(); i++, Sleep(m_duration * 1000 / m_text.length()))
+        {
+            std::cout << m_text[i];
+        }
+    }
+
+    float getTextSize() const
+    {
+        return m_text.length();
+    }
+
+    float getDuration() const
+    {
+        return m_duration;
+    }
+private:
+    std::string m_text;
+    float m_duration;
+    int currentChar = -1;
+};
+
+int main()
+{
+    AnimatedText animatedText("It was kinda difficult", 22);
+
+    //animatedText.DrawInConsole(); // логика работы (если нужен вывод текста в консоль)
+
+    // отрисовка с помощью графической библиотеки SFML
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Animated Text");
+    sf::Font font;
+    font.loadFromFile("arial.ttf");
+
+    sf::Text text;
+    text.setFont(font);
+    text.setCharacterSize(24);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        for (int i = 0; i < animatedText.getTextSize(); i++, Sleep(animatedText.getDuration() * 1000 / animatedText.getTextSize()))
+        {
+            animatedText.update();
+            text.setString(animatedText.getDisplayedText());
+            window.clear();
+            window.draw(text);
+            window.display();
+        }
+    }
+
+    return 0;
+}
